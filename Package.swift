8992a0587a6eea8c60ef1063a0e1877e7f97802b
yonihemi <jonathan@hemi.dev>
2022-08-31 18:21:35 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Exe",
     dependencies: [
+        .executable(name: "exe", targets: ["Exe"]),
     ],
     targets: [
         .executableTarget(
